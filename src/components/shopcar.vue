cea67d0214a08a4915383378cc52fbf07700010b
601460003 <601460003@qq.com>
2019-03-01 05:20:10 +0800

    修改了样式l

--- src/components/shopcar.vue
+++ src/components/shopcar.vue
@@ -29,7 +29,7 @@
           <!--v-model="checked"-->
           <van-checkbox >全选</van-checkbox>
           <span slot="tip">
-    你的收货地址不支持同城送, <span>修改地址</span>
+    你的收货地址不支持同城送, <span>修改地址.</span>
   </span>
         </van-submit-bar>
       </div>
---
+++ src/components/tabbers.vue
@@ -0,0 +1,73 @@
+<template>
+<div>
+  <van-tabbar v-model="active" style="z-index: 1999">
+    <van-tabbar-item
+      v-for="(tab, index) in tabbar"
+      :icon="tab.icon"
+      :to="tab.path"
+      :dot="tab.dot"
+      :info="tab.info"
+      :key="index">
+      {{tab.name}}
+    </van-tabbar-item>
+  </van-tabbar>
+</div>
+</template>
+
+<script>
+    export default {
+        name: "v-tabber",
+      data(){
+          return{
+            active: 0,
+            tabbar: [
+              {
+                name: '精选',
+                path: '/aheader',
+                pathName: 'aheader',
+                icon: 'wap-home',
+                dot: false,
+                info: ''
+              },
+              {
+                name: '专题',
+                path: '/towpage',
+                pathName: 'towpage',
+                icon: 'vip-card-o',
+                dot: false,
+                info: ''
+              },
+              {
+                name: '分类',
+                path: '/moreshop',
+                pathName: 'moreshop',
+                icon: 'orders-o',
+                dot: false,
+                info: ''
+              },
+              {
+                name: '购物车',
+                path: '/shopcar',
+                pathName: 'shopcar',
+                icon: 'shopping-cart-o',
+                dot: false,
+                info: ''
+              },
+              {
+                name: '个人',
+                path: '/me',
+                pathName: 'me',
+                icon: 'manager-o',
+                dot: false,
+                info: ''
+              }
+
+            ]
+          }
+      },
+    }
+</script>
+
+<style scoped>
+
+</style>
