6d968d719bb3ef9bbe9f2e9a95375b5c06302154
zhangzhiwei <601460003@qq.com>
2019-03-03 09:37:39 +0800

    分类

---
+++ src/views/home/home.vue
@@ -0,0 +1,209 @@
+<template>
+  <div class="hello">
+    <div class="title-aheader">
+      华升车行
+    </div>
+    <div style="background: white;padding: 0 10px">
+      <div style="text-align: center">
+
+      </div>
+      <div class="top-input">
+        <input type="text" placeholder="搜索商品，共21303款好货">
+      </div>
+      <!--//轮播图-->
+      <div class="slideshow">
+        <van-swipe :autoplay="3000" indicator-color="white" style="height: 200px">
+          <van-swipe-item v-for="(img,index) in Banner" :key="index">
+            <img :src="img.poster"
+                 width="100%" height="200px"/>
+          </van-swipe-item>
+        </van-swipe>
+      </div>
+      <div class="imgs-title">
+        <span>8小时内快速发货</span>
+        <span>30天内无忧退货</span>
+        <span>48小时内快速退货</span>
+      </div>
+    </div>
+    <div class="main-shop">
+      <!--机油坐标-->
+      <ul style="display: flex;flex-wrap: wrap;flex-direction: row">
+        <li v-for="goods in HotGoods" >
+          <div class="shop-left" @click="shopMSG" >
+            <img :src="goods.img"/>
+            <div class="goods-goods">
+              <span>{{goods.name}} <span class="goods-describe" style="color:#c8c9cc">{{goods.describe}}</span></span>
+            </div>
+            <span class="goods-price">￥{{goods.price}}</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <!--人气推介-->
+    <div style="background:white;">
+      <div style="padding: 10px;margin-top: 10px;color: rgb(85, 85, 85);border-bottom: 1px solid #F4F0EA;font-size: 15px">
+        <span style="float: right;padding-top: 2px">更多 ></span>
+        人气推荐
+      </div>
+      <div class="getChoice" v-for="hots in getChoice">
+        <div style="padding: 20px">
+          <img :src="hots.img" width="100px" height="100px"/>
+        </div>
+        <div>
+          <p style="color:#333;">{{hots.name}}</p>
+          <p style="color: #999;">{{hots.describe}}</p>
+          <span style="color: #f44;font-size: 16px">￥{{hots.price}}</span>
+        </div>
+      </div>
+    </div>
+    <!--专题精选-->
+    <div style="background: white;padding: 0 10px">
+      <div style="padding: 10px;margin-top: 10px;color: rgb(85, 85, 85);font-size: 15px">
+        <span style="float: right;padding-top: 2px" @click="moremore">更多 ></span>
+        专题精选
+      </div>
+      <div style="line-height: 10px;margin-bottom: 60px;margin-top: 5px">
+        <img src="https://img12.360buyimg.com/n1/jfs/t28771/205/431406045/382489/8f831c01/5bf39e46N6551e007.jpg"
+             width="100%" height="200px"/>
+        <div>
+          <span style="float: right;color: #f44;padding-right: 10px">159元起</span>
+          <p style="color: rgb(85, 85, 85)">朝阳轮胎</p>
+        </div>
+        <p style="color: #999;font-size: 14px">朝阳3:00-10大力神667无气可行驶 6层正空</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'home',
+    data() {
+      return {
+        Banner: [],
+        HotGoods: [],
+        getChoice: [],
+      }
+    },
+    created() {
+      // 第一个伦播图接口
+      this.$axios.get('home/getBanner')
+        .then(res => {
+          this.Banner = res.data.data;
+        });
+      // 第二个商品接口
+      this.$axios.get('home/getHotGoods')
+        .then(res => {
+          this.HotGoods = res.data.data;
+        });
+      // 第三个接口人气推介
+      this.$axios.get('home/getChoice')
+        .then(res => {
+          this.getChoice = res.data.data;
+          console.log(this.getChoice)
+        })
+    },
+
+    methods: {
+      moremore() {
+        this.$router.push()
+      },
+      shopMSG() {
+        this.$router.push({path: '/shopmsg'})
+      },
+    }
+  }
+</script>
+
+<style scoped>
+  .hello {
+    background: #dfe6ec;
+    font-family: ' Microsoft Yahei';
+  }
+
+  .goods-describe {
+    font-size: 12px;
+    color: #e3e3e3;
+  }
+
+  .title-aheader {
+    text-align: center;
+    padding: 10px;
+    font-size: 24px;
+    background-color: #f44;
+    color: #fff;
+  }
+
+  .top-input input {
+    width: 100%;
+    margin: 10px auto;
+    text-align: center;
+    color: red;
+    font-size: 20px;
+    padding-top: 5px;
+    border-radius: 15px;
+    border: 1px solid #999;
+  }
+
+  .imgs-title {
+    font-size: 18px;
+    color: rgb(162, 49, 62);
+    display: flex;
+    justify-content: space-around;
+    margin-top: 3px;
+    padding: 10px;
+    background: white;
+  }
+
+  .imgs-title span {
+    /*background: url("../../assets/radio button.png") no-repeat;*/
+    background-size: 15px;
+    padding-left: 18px;
+  }
+
+  .main-shop {
+    margin-top: 10px;
+  }
+
+  .shop-left {
+    text-align: center;
+    padding-top: 20px;
+    background: white;
+    width: 185px;
+  }
+
+  .shop-left img {
+    padding-bottom: 5px;
+    width: 120px;
+    height: 120px;
+  }
+
+  .shop-left h4, h5 {
+    padding: 0;
+    margin: 0;
+    font-weight: normal;
+    font-size: 22px;
+    color: #333;
+  }
+
+  .goods-price{
+    color: #f44;
+    padding-right: 20px;
+    font-size: 24px;
+  }
+
+  .getChoice {
+    background: white;
+    display: flex;
+    border-bottom: 1px solid #F4F0EA;
+  }
+
+  .goods-goods {
+    display: -webkit-box;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+  }
+</style>
